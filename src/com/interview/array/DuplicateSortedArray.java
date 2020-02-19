4ace4d25ecc40c309b415041264709e2f6f4a822
songyi <songyi@meituan.com>
2020-02-19 09:33:24 +0800

    选择排序、插入排序、排序数组删除重复

--- src/com/interview/array/DuplicateSortedArray.java
+++ src/com/interview/array/DuplicateSortedArray.java
@@ -1,9 +1,64 @@
 package com.interview.array;
 
+import java.util.Arrays;
+
 /**
  * @author songyi
  * @date 2020-02-19 08:05
- * @Description:
+ * @Description:https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * 循环一遍，记录不重复的个数，如果发现不重复就把这个数放到 索引上，索引+1，最后索引值就是个数
+ *
+ * 不重复的个数就是 相邻两个数不相等 的个数，找到不重复就往前放
  */
 public class DuplicateSortedArray {
+
+    //删除排序好的重复数据
+
+    public static void main(String[] args) {
+        int[] num ={1,1};
+        DuplicateSortedArray duplicateSortedArray = new DuplicateSortedArray();
+        duplicateSortedArray.removeDuplicates(num);
+    }
+
+    //更优雅的写法
+    //链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shan-chu-pai-xu-shu-zu-zhong-de-zhong-fu-xiang-by-/
+    public int removeDuplicatesRef(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 1){
+            return 1;
+        }
+        int result = 1;
+        //int duplicateNum = 0;
+        int index = 1;
+        int temp = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            //不重复
+             if(nums[i] != temp){
+                 temp = nums[i];
+                 result++;
+                 nums[index++] = nums[i];
+             }
+//             else {
+//                 //重复数据
+//                 duplicateNum++;
+//             }
+
+        }
+        System.out.println(Arrays.toString(nums));
+
+        return result;
+
+    }
 }
--- src/com/interview/sort/InsertSort.java
+++ src/com/interview/sort/InsertSort.java
@@ -1,9 +1,45 @@
 package com.interview.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author songyi
  * @date 2020-02-19 07:20
  * @Description:
  */
 public class InsertSort {
+
+    public static void main(String[] args) {
+        //int[] a = {3,1,4,2,8};
+        int[] a = {1};
+        InsertSort insertSort = new InsertSort();
+        insertSort.insertSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public void insertSort(int[] arr){
+
+        for(int i = 1;  i < arr.length; i++){
+            //假定前i个按照从小到大排好序了， 前i个最大值是 arr[i-1]
+            int insertVale = arr[i];
+            int index = i - 1;
+            //第i个是待插入的数据， 从第 i-1个开始比较，如果比 i-1个小， i-1个后移，继续和i-2个比较，如果比i-2个小，继续后移
+            while (index >= 0 && insertVale < arr[index]){
+                //第一个 index + 1 = i
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            //最后找到了插入的位置，插入即可
+            arr[index+1] = insertVale;
+        }
+
+        List<Integer> ls= IntStream.of(arr).boxed().collect(Collectors.toList());
+        System.out.println(ls);
+
+    }
 }
